6d7a5e70c3f357119a5ef6cb68ed8a0668a5d21e
Henry B <noelys215@gmail.com>
2022-10-01 14:28:17 -0400

    add update user functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,9 @@ import {
 	getUserProfile,
 	updateUserProfile,
 	getUsers,
+	deleteUser,
+	getUserById,
+	updateUser,
 } from '../controllers/userController.js';
 const router = express.Router();
 import { admin, protect } from '../middleware/authMiddleware.js';
@@ -12,5 +15,10 @@ import { admin, protect } from '../middleware/authMiddleware.js';
 router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router
+	.route('/:id')
+	.delete(protect, admin, deleteUser)
+	.get(protect, admin, getUserById)
+	.put(protect, admin, updateUser);
 
 export default router;
