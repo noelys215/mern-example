dcf73f0caddb377cf4993ff8b9ad6c97adbbbae8
Henry B <noelys215@gmail.com>
2022-10-02 18:17:26 -0400

    merge fixes

---
+++ frontend/src/App.js
@@ -0,0 +1,56 @@
+import Header from './components/Header.component';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer.component';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+
+const App = () => {
+	return (
+		<Router>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<Routes>
+						<Route path="/product/:id" element={<ProductScreen />} />
+						<Route path="/" element={<HomeScreen />} />
+						<Route path="/cart" element={<CartScreen />} />
+						<Route path="/cart/:id" element={<CartScreen />} />
+						<Route path="/login" element={<LoginScreen />} />
+						<Route path="/register" element={<RegisterScreen />} />
+						<Route path="/shipping" element={<ShippingScreen />} />
+						<Route path="/payment" element={<PaymentScreen />} />
+						<Route path="/placeorder" element={<PlaceOrderScreen />} />
+
+						<Route path="/order/:id" element={<OrderScreen />} />
+						<Route path="/profile" element={<ProfileScreen />} />
+
+						{/* <Route path="/cart">
+							<Route path=":id" element={<CartScreen />} />
+							<Route path="" element={<CartScreen />} />
+						</Route> */}
+						<Route path="/admin/userlist" element={<UserListScreen />} />
+						<Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+						<Route path="/admin/productlist" element={<ProductListScreen />} exact />
+						<Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+					</Routes>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	);
+};
+
+export default App;
---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,117 @@
+import { useEffect } from 'react';
+import { Link, useNavigate, useLocation, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import { addToCart, removeFromCart } from '../actions/cartActions';
+
+const CartScreen = ({ match, location }) => {
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const qty = new URLSearchParams(useLocation().search).get('qty');
+	const dispatch = useDispatch();
+
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
+	useEffect(() => {
+		if (id) {
+			dispatch(addToCart(id, qty));
+		}
+	}, [dispatch, id, qty]);
+
+	const removeFromCartHandler = (id) => {
+		dispatch(removeFromCart(id));
+		navigate('/cart');
+	};
+
+	const checkoutHandler = () => {
+		if (userInfo) {
+			navigate('/shipping');
+		} else {
+			navigate('/login');
+		}
+	};
+
+	return (
+		<Row>
+			<Col md={8}>
+				<h1>Shopping Cart</h1>
+				{cartItems.length === 0 ? (
+					<Message>
+						Your Cart Is Empty... <Link to={'/'}>Go Back</Link>
+					</Message>
+				) : (
+					<ListGroup variant="flush">
+						{cartItems.map((item) => (
+							<ListGroup.Item key={item.product}>
+								<Row>
+									<Col md={2}>
+										<Image src={item.image} alt={item.name} fluid rounded />
+									</Col>
+									<Col md={3}>
+										<Link to={`/product/${item.product}`}>{item.name}</Link>
+									</Col>
+									<Col md={2}>${item.price}</Col>
+									<Col md={2} xs={6}>
+										<Form.Control
+											as="select"
+											value={item.qty}
+											onChange={(e) =>
+												dispatch(
+													addToCart(item.product, Number(e.target.value))
+												)
+											}>
+											{[...Array(item.countInStock).keys()].map((x) => (
+												<option key={x + 1} value={x + 1}>
+													{x + 1}
+												</option>
+											))}
+										</Form.Control>
+									</Col>
+									<Col md={2} xs={6}>
+										<Button
+											type="button"
+											variant="light"
+											onClick={() => removeFromCartHandler(item.product)}>
+											<i className="fas fa-trash" />
+										</Button>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+						))}
+					</ListGroup>
+				)}
+			</Col>
+			<Col md={4}>
+				<Card>
+					<ListGroup variant="flush">
+						<ListGroup.Item>
+							<h2>
+								Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+								items
+							</h2>
+							$
+							{cartItems
+								.reduce((acc, item) => acc + item.qty * item.price, 0)
+								.toFixed(2)}
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Button
+								type="button"
+								style={{ width: '100%' }}
+								disabled={!cartItems}
+								onClick={checkoutHandler}>
+								Proceed to Checkout
+							</Button>
+						</ListGroup.Item>
+					</ListGroup>
+				</Card>
+			</Col>
+		</Row>
+	);
+};
+
+export default CartScreen;
